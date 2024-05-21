5234a9a4e52062712f2b0b0e96d7f4599364a135
김명구 <myeonggu.kim@kakao.com>
2024-05-21 15:26:53 +0900

    코드 정리: kafka property key 는 라이브러리의 상수 사용

--- java-8-kafka/src/main/java/samples/kafka/TopicProducer.java
+++ java-8-kafka/src/main/java/samples/kafka/TopicProducer.java
@@ -1,10 +1,9 @@
 package samples.kafka;
 
-import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,6 +12,7 @@ import org.json.JSONObject;
 public class TopicProducer {
   
   public static final String BOOTSTRAP_SERVERS = "172.28.200.30:9092";
+  public static final String GROUP_ID = "mygroup";
   private Properties props = new Properties();
   
   public TopicProducer() {
@@ -20,9 +20,10 @@ public class TopicProducer {
   }
   
   public TopicProducer(String bootstrapServers) {
-    props.put("bootstrap.servers", bootstrapServers);
-    props.put("key.serializer", org.apache.kafka.common.serialization.StringSerializer.class);
-    props.put("value.serializer", org.apache.kafka.common.serialization.StringSerializer.class);
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
   }
   
   public void send(String topic, Map<String, Object> data) {
@@ -30,6 +31,7 @@ public class TopicProducer {
     JSONObject json = new JSONObject();
     json.put("data", data);
     String message = json.toString();
+    // cluster 모드일 경우 key 값에 대한 고려가 필요합니다.
     producer.send(new ProducerRecord<String, String>(topic, message));
     producer.close();
   }
