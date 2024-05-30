1afcd161bf50f92ae2f641bf0e88112216d1dac9
김명구 <myeonggu.kim@kakao.com>
2024-05-30 17:01:42 +0900

    datasource 멀티 구성 예시 추가

--- springboot-3.x-mybatis/src/main/java/spring/sample/mybatis/config/DataSourceConfig.java
+++ springboot-3.x-mybatis/src/main/java/spring/sample/mybatis/config/DataSourceConfig.java
@@ -25,9 +25,17 @@ public class DataSourceConfig {
   
   Logger log = LoggerFactory.getLogger(DataSourceConfig.class);
   
-  @Bean(name = "main")
-  @ConfigurationProperties(prefix = "sample.jdbc.datasource")
-  DataSource dataSource() throws SQLException {
+  @Bean(name = "mssql")
+  @ConfigurationProperties(prefix = "sample.jdbc.datasource.mssql")
+  DataSource dataSource_mssql() throws SQLException {
+    return DataSourceBuilder.create()
+        .type(HikariDataSource.class)
+        .build();
+  }
+  
+  @Bean(name = "oracle")
+  @ConfigurationProperties(prefix = "sample.jdbc.datasource.oracle")
+  DataSource dataSource_oracle() throws SQLException {
     return DataSourceBuilder.create()
         .type(HikariDataSource.class)
         .build();
@@ -35,12 +43,14 @@ public class DataSourceConfig {
   
   @Bean
   RoutingDataSource routingDataSource(
-      @Qualifier("main") DataSource mainDatasource) {
+      @Qualifier("mssql") DataSource mssqlDatasource,
+      @Qualifier("oracle") DataSource oracleDatasource) {
     RoutingDataSource routingDataSource = new RoutingDataSource();
     Map<Object, Object> dataSources = new HashMap<Object, Object>();
-    dataSources.put("main", mainDatasource);
+    dataSources.put("mssql", mssqlDatasource);
+    dataSources.put("oracle", oracleDatasource);
     routingDataSource.setTargetDataSources(dataSources);
-    routingDataSource.setDefaultTargetDataSource(mainDatasource);
+    routingDataSource.setDefaultTargetDataSource(mssqlDatasource);
     return routingDataSource;
   }
   
@@ -67,7 +77,9 @@ public class DataSourceConfig {
     
     @Override
     protected Object determineCurrentLookupKey() {
-      String routingKey = "main";
+      // 특정 사용자, 특정 서비스 등에 따라 datasource 가 결정되도록 할 수 있습니다. 
+      // mssql datasource 를 기본값으로 설정했습니다.
+      String routingKey = "mssql";
       return routingKey;
     }
     
@@ -78,7 +90,7 @@ public class DataSourceConfig {
     }
   }
   
-  @Bean
+  @Bean(name = "lazyConnectionDataSourceProxy")
   LazyConnectionDataSourceProxy lazyConnectionDataSourceProxy(
       RoutingDataSource routingDataSource) {
     return new LazyConnectionDataSourceProxy(routingDataSource);
--- springboot-3.x-mybatis/src/main/java/spring/sample/mybatis/config/MybatisConfig.java
+++ springboot-3.x-mybatis/src/main/java/spring/sample/mybatis/config/MybatisConfig.java
@@ -1,5 +1,7 @@
 package spring.sample.mybatis.config;
 
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.ExecutorType;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -30,24 +32,38 @@ public class MybatisConfig {
   Logger log = LoggerFactory.getLogger(MybatisConfig.class);
   
   public static final String BASE_PACKAGES = "spring";
-
-  @Autowired
-  LazyConnectionDataSourceProxy dataSource;
   
   @Autowired
   MybatisProperties properties;
   
-  @Bean
-  SqlSessionFactoryBean sqlSessionFactoryBean() throws Exception {
+  @Autowired
+  @Qualifier("lazyConnectionDataSourceProxy")
+  LazyConnectionDataSourceProxy lazyConnectionDataSourceProxy;
+  
+  @Autowired
+  @Qualifier("mssql")
+  DataSource dataSource_mssql;
+  
+  @Autowired
+  @Qualifier("oracle")
+  DataSource dataSource_oracle;
+  
+  // mssql
+  @Bean(name = "sqlSessionFactoryBean_mssql")
+  @Primary
+  SqlSessionFactoryBean sqlSessionFactoryBean_mssql() throws Exception {
     String configFile = properties.getConfigFile();
     String mapperLocation = properties.getMapperLocation();
     String typeAliasesPackage = properties.getTypeAliasesPackage();
-    log.info("configFile: " + configFile);
-    log.info("mapperLocation: " + mapperLocation);
-    log.info("typeAliasesPackage: " + typeAliasesPackage);
+    log.info("[mssql] configFile: " + configFile);
+    log.info("[mssql] mapperLocation: " + mapperLocation);
+    log.info("[mssql] typeAliasesPackage: " + typeAliasesPackage);
     
     SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-    sqlSessionFactoryBean.setDataSource(dataSource);
+    // RoutingDataSource 로 runtime 시 datasource 를 결정해야 할 경우 아래 코드 사용
+    // sqlSessionFactoryBean.setDataSource(lazyConnectionDataSourceProxy);
+    sqlSessionFactoryBean.setDataSource(dataSource_mssql);
+    
     // mybatis.xml 을 사용할 경우
     sqlSessionFactoryBean.setConfigLocation(
         new PathMatchingResourcePatternResolver().getResource(configFile));
@@ -64,21 +80,72 @@ public class MybatisConfig {
   }
   
   @Bean
-  DaoSupport daoSupport(
-      @Qualifier("sqlSessionTemplate") SqlSessionTemplate sqlSessionTemplate) throws Exception {
+  @Primary
+  DaoSupport daoSupport_mssql(
+      @Qualifier("sqlSessionTemplate_mssql") SqlSessionTemplate sqlSessionTemplate) throws Exception {
     return new DaoSupport(sqlSessionTemplate);
   }
   
+  @Bean(name = "sqlSessionTemplate_mssql")
   @Primary
-  @Bean(name = "sqlSessionTemplate")
-  SqlSessionTemplate sqlSessionTemplate(
-      SqlSessionFactoryBean sqlSessionFactoryBean) throws Exception {
+  SqlSessionTemplate sqlSessionTemplate_mssql(
+      @Qualifier("sqlSessionFactoryBean_mssql") SqlSessionFactoryBean sqlSessionFactoryBean) throws Exception {
      return new SqlSessionTemplate(sqlSessionFactoryBean.getObject());
   }
   
-  @Bean(name = "sqlSessionTemplateForBatchExecutor")
-  SqlSessionTemplate sqlSessionTemplateForBatchExecutor(
-      SqlSessionFactoryBean sqlSessionFactoryBean) throws Exception {
+  @Bean(name = "sqlSessionTemplateForBatchExecutor_mssql")
+  SqlSessionTemplate sqlSessionTemplateForBatchExecutor_mssql(
+      @Qualifier("sqlSessionFactoryBean_mssql") SqlSessionFactoryBean sqlSessionFactoryBean) throws Exception {
+    return new SqlSessionTemplate(sqlSessionFactoryBean.getObject(), ExecutorType.BATCH);
+  }
+  
+  
+  
+  // oracle
+  @Bean(name = "sqlSessionFactoryBean_oracle")
+  SqlSessionFactoryBean sqlSessionFactoryBean_oracle() throws Exception {
+    String configFile = properties.getConfigFile();
+    String mapperLocation = properties.getMapperLocation();
+    String typeAliasesPackage = properties.getTypeAliasesPackage();
+    log.info("[oracle] configFile: " + configFile);
+    log.info("[oracle] mapperLocation: " + mapperLocation);
+    log.info("[oracle] typeAliasesPackage: " + typeAliasesPackage);
+    
+    SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+    // RoutingDataSource 로 runtime 시 datasource 를 결정해야 할 경우 아래 코드 사용
+    // sqlSessionFactoryBean.setDataSource(routingDataSource);
+    sqlSessionFactoryBean.setDataSource(dataSource_oracle);
+    
+    // mybatis.xml 을 사용할 경우
+    sqlSessionFactoryBean.setConfigLocation(
+        new PathMatchingResourcePatternResolver().getResource(configFile));
+    sqlSessionFactoryBean.setMapperLocations(
+        new PathMatchingResourcePatternResolver().getResources(mapperLocation));
+    // parameterType, resultType 에 java package 생략 가능
+    // 단, BASE_PACKAGES 하위 package 에 동일한 클래스명이 2개 이상일 경우 어플리케이션 booting 시 오류가 발생함
+    if (!ObjectUtils.isEmpty(typeAliasesPackage)) {
+      sqlSessionFactoryBean.setTypeAliasesPackage(typeAliasesPackage);
+    }
+    // 페이징 처리를 위한 mybatis-plugin 추가
+    sqlSessionFactoryBean.setPlugins(new spring.sample.mybatis.config.mybatis.PagingInterceptor());
+    return sqlSessionFactoryBean;
+  }
+  
+  @Bean
+  DaoSupport daoSupport_oracle(
+      @Qualifier("sqlSessionTemplate_oracle") SqlSessionTemplate sqlSessionTemplate) throws Exception {
+    return new DaoSupport(sqlSessionTemplate);
+  }
+  
+  @Bean(name = "sqlSessionTemplate_oracle")
+  SqlSessionTemplate sqlSessionTemplate_oracle(
+      @Qualifier("sqlSessionFactoryBean_oracle") SqlSessionFactoryBean sqlSessionFactoryBean) throws Exception {
+    return new SqlSessionTemplate(sqlSessionFactoryBean.getObject());
+  }
+  
+  @Bean(name = "sqlSessionTemplateForBatchExecutor_oracle")
+  SqlSessionTemplate sqlSessionTemplateForBatchExecutor_oracle(
+      @Qualifier("sqlSessionFactoryBean_oracle") SqlSessionFactoryBean sqlSessionFactoryBean) throws Exception {
     return new SqlSessionTemplate(sqlSessionFactoryBean.getObject(), ExecutorType.BATCH);
   }
 }
