457ff12c3c9708ce8c6ff1b6d5613a0d758c6884
김명구 <myeonggu.kim@kakao.com>
2024-08-03 21:14:26 +0900

    elastic 추가

---
+++ verify/src/main/java/verify/Main.java
@@ -0,0 +1,30 @@
+package verify;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class Main {
+  
+  public static void main(String[] args) throws IOException {
+    String phone = "010   abc5678";
+    
+    byte[] raw = phone.getBytes();
+    
+    ByteArrayInputStream is = new ByteArrayInputStream(raw);
+    
+    byte[] first = new byte[3];
+    byte[] second = new byte[6];
+    byte[] third = new byte[4];
+    
+    is.read(first);
+    is.read(second);
+    is.read(third);
+    
+    System.out.println(new String(first));
+    System.out.println(new String(second));
+    System.out.println(new String(third));
+    
+    System.out.println("["+new String(new byte[] {0x30})+"]");
+    System.out.println("["+new String(new byte[] {0x20})+"]");
+  }
+}
