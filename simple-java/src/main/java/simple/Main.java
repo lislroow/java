f80a920ecce3ecbbce985bdb2a34364560039b15
김명구 <myeonggu.kim@kakao.com>
2024-08-14 12:08:10 +0900

    프로젝트 정리

--- simple-java/src/main/java/simple/Main.java
+++ simple-java/src/main/java/simple/Main.java
@@ -1,4 +1,4 @@
-package verify;
+package simple;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
--- simple-springboot/src/main/java/simple/MainApplication.java
+++ simple-springboot/src/main/java/simple/MainApplication.java
@@ -1,4 +1,4 @@
-package app;
+package simple;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
