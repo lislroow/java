1a1dee8aac8e1e153cbd7cdc746f0b9807d4d0b7
김명구 <myeonggu.kim@kakao.com>
2024-08-15 17:56:00 +0900

    프로젝트명 전체 리팩토링: feignclient name 에는 언더스코어(_)가 포함될 수 없음

---
+++ springboot23-app/src/main/java/spring/sample/app/feign/EgressFeign.java
@@ -0,0 +1,18 @@
+package spring.sample.app.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import spring.sample.app.code.SOAP_CLIENT_TYPE;
+import spring.sample.app.feign.dto.WebServiceDTO;
+
+@FeignClient(name = "springboot23-egress", url = "/")
+public interface EgressFeign {
+
+  @GetMapping("/v1/internal/egress/soap/{soapClientType}")
+  public ResponseEntity<WebServiceDTO.MemberVO> soap(
+      @PathVariable(name = "soapClientType") SOAP_CLIENT_TYPE soapClientType);
+  
+}
