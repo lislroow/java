f461408754bab20c1cdc57676cfe50c2a72ba95a
김명구 <myeonggu.kim@kakao.com>
2024-09-03 15:11:54 +0900

    distinctByKey

--- java8/src/main/java/samples/stream/SampleStream.java
+++ java8/src/main/java/samples/stream/SampleStream.java
@@ -3,6 +3,9 @@ package samples.stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import lombok.AllArgsConstructor;
@@ -16,21 +19,41 @@ public class SampleStream {
   @AllArgsConstructor
   @NoArgsConstructor
   @Builder
-  static class DummyVo {
+  static class ItemVo {
     private String id;
     private String subId;
   }
+  @Data
+  @AllArgsConstructor
+  static class GroupVo {
+    private String id;
+  }
   
   public static void main(String[] args) {
-    List<DummyVo> list = Arrays.asList(
-        DummyVo.builder().id("Foo").subId("Foo-1").build(),
-        DummyVo.builder().id("Foo").subId("Foo-2").build(),
-        DummyVo.builder().id("Foo").subId("Foo-3").build(),
-        DummyVo.builder().id("Bar").subId("Bar-1").build(),
-        DummyVo.builder().id("Bar").subId("Bar-2").build()
+    List<ItemVo> list = Arrays.asList(
+        ItemVo.builder().id("Foo").subId("Foo-1").build(),
+        ItemVo.builder().id("Foo").subId("Foo-2").build(),
+        ItemVo.builder().id("Foo").subId("Foo-3").build(),
+        ItemVo.builder().id("Bar").subId("Bar-1").build(),
+        ItemVo.builder().id("Bar").subId("Bar-2").build()
         );
-    Map<String, List<DummyVo>> map = list.stream().collect(Collectors.groupingBy(DummyVo::getId));
+    Map<String, List<ItemVo>> map = list.stream().collect(Collectors.groupingBy(ItemVo::getId));
     
     map.values().stream().forEach(item -> System.out.println(item));
+    
+    System.out.println("----");
+    
+    list.stream().collect(Collectors.groupingBy(ItemVo::getId, Collectors.toSet()));
+    List<GroupVo> distList = list.stream().filter(distinctByKey(ItemVo::getId))
+        .map(item -> new GroupVo(item.getId()))
+        .collect(Collectors.toList());
+    System.out.println(distList);
   }
+  
+  public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
+      Map<Object, Boolean> map = new ConcurrentHashMap<>();
+      return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+  }
+
+
 }
