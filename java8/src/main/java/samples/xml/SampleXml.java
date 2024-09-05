ea6e6af1a211a94678c26cfcf92fa6a4f1673937
김명구 <myeonggu.kim@kakao.com>
2024-09-05 23:30:31 +0900

    xml 예시 추가: 코드 정리

--- java8/src/main/java/samples/xml/SampleXml.java
+++ java8/src/main/java/samples/xml/SampleXml.java
@@ -1,25 +1,22 @@
 package samples.xml;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -33,18 +30,17 @@ public class SampleXml {
     XML_STR1,
     XML_STR2,
     XML_STR3,
-    XML_STR4,
-    XML_STR5;
+    XML_STR4;
     
     String xmlstr;
     
     TestXmlString() {
       String xmlstr = "";
-      int ordinal = this.ordinal();
+      int idx = this.ordinal()+1;
       String name = this.name();
-      log.trace(String.format("[%d] %s", ordinal, name));
-      switch (ordinal) {
-      case 0:
+      log.trace(String.format("[%d] %s", idx, name));
+      switch (idx) {
+      case 1:
         xmlstr = "";
         xmlstr += "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">";
         xmlstr += "  <soap:Header>";
@@ -74,7 +70,7 @@ public class SampleXml {
         xmlstr += "  </soap:Body>";
         xmlstr += "</soap:Envelope>";
         break;
-      case 1:
+      case 2:
         xmlstr = "";
         xmlstr += "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">";
         xmlstr += "  <soap:Header>";
@@ -96,7 +92,7 @@ public class SampleXml {
         xmlstr += "  </soap:Body>";
         xmlstr += "</soap:Envelope>";
         break;
-      case 2:
+      case 3:
         xmlstr = "";
         xmlstr += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
         xmlstr += "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">";
@@ -123,7 +119,7 @@ public class SampleXml {
         xmlstr += "  </soap:Body>";
         xmlstr += "</soap:Envelope>";
         break;
-      case 3:
+      case 4:
         xmlstr = "";
         xmlstr += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
         xmlstr += "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">";
@@ -159,27 +155,6 @@ public class SampleXml {
         xmlstr += "    </getReductionFarmMngYnResponse>";
         xmlstr += "  </soap:Body>";
         xmlstr += "</soap:Envelope>";
-        break;
-      case 4:
-        xmlstr = "";
-        xmlstr += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-        xmlstr += "<Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">";
-        xmlstr += "  <Header>";
-        xmlstr += "    <commonHeader xmlns=\"http://ccais.mopas.go.kr/dh/rid/services/ctr/ReductionFarmMngYn/types\">";
-        xmlstr += "      <serviceName>ReductionFarmMngYnService</serviceName>";
-        xmlstr += "      <useSystemCode>ESBTEST</useSystemCode>";
-        xmlstr += "      <certServerId>SVR1311000030</certServerId>";
-        xmlstr += "      <transactionUniqueId>2010111020382700773722611</transactionUniqueId>";
-        xmlstr += "      <userDeptCode>1234567</userDeptCode>";
-        xmlstr += "      <userName>김공무</userName>";
-        xmlstr += "    </commonHeader>";
-        xmlstr += "  </Header>";
-        xmlstr += "  <Body>";
-        xmlstr += "    <getReductionFarmMngYn xmlns=\"http://ccais.mopas.go.kr/dh/rid/services/ctr/ReductionFarmMngYn/types\">";
-        xmlstr += "      <jumin>주민등록번호</jumin>";
-        xmlstr += "    </getReductionFarmMngYn>";
-        xmlstr += "  </Body>";
-        xmlstr += "</Envelope>";
         break;
       }
       this.xmlstr = xmlstr;
@@ -217,162 +192,130 @@ public class SampleXml {
     }
   }
   
-  public static void test1(TestXmlString xml) {
+  public static void convertToMap(TestXmlString xml) throws Exception {
     String xmlstr = xml.getXmlStr();
-    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    factory.setNamespaceAware(false);
-    DocumentBuilder builder = null;
-    try {
-      builder = factory.newDocumentBuilder();
-    } catch (ParserConfigurationException e) {
-      e.printStackTrace();
-    }
-    Document document = null;
-    try {
-      document = builder.parse(new java.io.ByteArrayInputStream(xmlstr.getBytes("UTF-8")));
-    } catch (SAXException | IOException e) {
-      e.printStackTrace();
-    }
-
-    Element root = document.getDocumentElement();
-    XPathFactory xpathFactory = XPathFactory.newInstance();
-    XPath xpath = xpathFactory.newXPath();
-    
+    String pathstr = null;
     String serviceName = null;
     
-    String pathstr = "";
-    pathstr = "/Envelope/Header/commonHeader/*";
-    NodeList headerNode = null;
-    try {
-      headerNode = (NodeList) xpath.compile(pathstr).evaluate(document, XPathConstants.NODESET);
-    } catch (XPathExpressionException e) {
-      e.printStackTrace();
-    }
-    int len = headerNode.getLength();
-    log.debug(pathstr + " ["+len+"]");
-    for (int i=0; i<len; i++) {
-      Node node = headerNode.item(i);
-      if (node.getNodeType() == Node.ELEMENT_NODE) {
-        log.trace(node.getNodeName() + " : " + node.getTextContent());
-      }
-      if ("serviceName".equals(node.getNodeName())) {
-        serviceName = node.getTextContent();
-        serviceName = serviceName.substring(0, serviceName.lastIndexOf("Service"));
-      }
+    Map<String, Object> resultMap = new HashMap<>();
+    pathstr = "/Envelope/Header/commonHeader/serviceName";
+    Optional<String> val = getValueByPath(xmlstr, pathstr);
+    if (!val.isPresent()) {
+      log.error("'%s' not found", pathstr);
+      return;
     }
     
-    log.info(String.format("serviceName: %s", serviceName));
-    log.debug("\n---\n");
+    serviceName = val.get();
+    serviceName = serviceName.substring(0, serviceName.lastIndexOf("Service"));
+    pathstr = "/Envelope/Header/commonHeader";
+    resultMap.put("header", getMapByPath(xmlstr, pathstr));
+    pathstr = "/Envelope/Body/get"+serviceName+"Response";
+    resultMap.put("body", getMapByPath(xmlstr, pathstr));
     
-    pathstr = String.format("/Envelope/Body/%s%s%s/*", "get", serviceName, "Response");
-    NodeList bodyNode = null;
-    try {
-      bodyNode = (NodeList) xpath.compile(pathstr).evaluate(document, XPathConstants.NODESET);
-    } catch (XPathExpressionException e) {
-      e.printStackTrace();
-    }
-    len = bodyNode.getLength();
-    log.trace(pathstr + " ["+len+"]");
-    for (int i=0; i<len; i++) {
-      Node node = bodyNode.item(i);
-      if (node.getNodeType() == Node.ELEMENT_NODE) {
-        log.trace(node.getNodeName() + " : " + node.getTextContent());
-      }
-      String nodeName = node.getNodeName();
-      int childrenLen = node.getChildNodes().getLength();
-      boolean hasChildren = node.hasChildNodes();
-      NodeList childrenNodes = node.getChildNodes();
-      for (int j=0; j<childrenLen; j++) {
-        if (childrenNodes.item(j).getNodeType() == Node.ELEMENT_NODE) {
-          String name = childrenNodes.item(j).getNodeName();
-          String text = childrenNodes.item(j).getTextContent();
-          log.trace(String.format("\"%s\"=\"%s\"", name, text));
-        }
-      }
-    }
-    
-    log.debug("\n---\n");
-    
-    xpath = xpathFactory.newXPath();
-    List<String> pathList = new ArrayList<String>();
-    extractPath(root, "", xpath, pathList);
-    pathList.forEach(item -> log.trace(item));
-    
-    log.debug("\n---\n");
-  }
-  
-  public static void test(TestXmlString xml) {
-    String xmlstr = xml.getXmlStr();
-    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    factory.setNamespaceAware(false);
-    DocumentBuilder builder = null;
-    try {
-      builder = factory.newDocumentBuilder();
-    } catch (ParserConfigurationException e) {
-      e.printStackTrace();
-    }
-    Document document = null;
-    try {
-      document = builder.parse(new java.io.ByteArrayInputStream(xmlstr.getBytes("UTF-8")));
-    } catch (SAXException | IOException e) {
-      e.printStackTrace();
-    }
-    Element root = document.getDocumentElement();
-    
-    Map<String, Object> jsonMap = new HashMap<>();
-    buildJsonMap(root, jsonMap);
-    log.debug("jsonMap: {}", jsonMap);
-    
+    log.debug("map: {}", resultMap);
     ObjectMapper objectMapper = new ObjectMapper();
     try {
-      String jsonstr = objectMapper.writeValueAsString(jsonMap);
-      log.info("jsonstr: {}", jsonstr);
+      String jsonstr = objectMapper.writeValueAsString(resultMap);
+      log.info("{}", jsonstr);
     } catch (JsonProcessingException e) {
       e.printStackTrace();
     }
-    
   }
   
-  private static void buildJsonMap(Node node, Map<String, Object> currentMap) {
+  public static Map<String, Object> getMapByPath(String xmlstr, String pathstr) {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    factory.setNamespaceAware(false);
+    DocumentBuilder builder = null;
+    Document document = null;
+    try {
+      builder = factory.newDocumentBuilder();
+      document = builder.parse(new java.io.ByteArrayInputStream(xmlstr.getBytes("UTF-8")));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    Map<String, Object> map = new HashMap<>();
+    XPathFactory xpathFactory = XPathFactory.newInstance();
+    XPath xpath = xpathFactory.newXPath();
+    Node node = null;
+    try {
+      node = (Node) xpath.compile(pathstr).evaluate(document, XPathConstants.NODE);
+    } catch (XPathExpressionException e) {
+      e.printStackTrace();
+    }
+    toMap(node, map);
+    return map;
+  }
+  
+  public static Optional<String> getValueByPath(String xmlstr, String pathstr) {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    factory.setNamespaceAware(false);
+    DocumentBuilder builder = null;
+    Document document = null;
+    try {
+      builder = factory.newDocumentBuilder();
+      document = builder.parse(new java.io.ByteArrayInputStream(xmlstr.getBytes("UTF-8")));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    String value = null;
+    XPathFactory xpathFactory = XPathFactory.newInstance();
+    XPath xpath = xpathFactory.newXPath();
+    Node node = null;
+    try {
+      node = (Node) xpath.compile(pathstr).evaluate(document, XPathConstants.NODE);
+    } catch (XPathExpressionException e) {
+      e.printStackTrace();
+    }
+    NodeList children = node.getChildNodes();
+    if (children.getLength() == 1 && children.item(0).getNodeType() == Node.TEXT_NODE) {
+      value = children.item(0).getTextContent().trim();
+    }
+    return Optional.ofNullable(value);
+  }
+  
+  private static void toMap(Node node, Map<String, Object> map) {
     NodeList children = node.getChildNodes();
     for (int i=0; i<children.getLength(); i++) {
       Node childNode = children.item(i);
       if (childNode.getNodeType() == Node.ELEMENT_NODE) {
-        String name = node.getNodeName();
-        // ns 제거
-        if (name.indexOf(":") > 0) {
-          name = name.substring(name.indexOf(":")+1, name.length());
-        }
-        String childName = name;
-        if (currentMap.containsKey(childName)) {
-          Object existingValue = currentMap.get(childName);
+        String childName = childNode.getNodeName();
+        if (map.containsKey(childName)) {
+          Object existingValue = map.get(childName);
           if (existingValue instanceof List) {
-            ((List<Object>)existingValue).add(buildChildNodeMap(childNode));
+            ((List<Object>)existingValue).add(getText(childNode));
           } else {
             List<Object> newList = new ArrayList<>();
             newList.add(existingValue);
-            newList.add(buildChildNodeMap(childNode));
-            currentMap.put(childName, newList);
+            newList.add(getText(childNode));
+            map.put(childName, newList);
           }
         } else {
-          currentMap.put(childName, buildChildNodeMap(childNode));
+          map.put(childName, getText(childNode));
         }
       }
     }
   }
   
-  private static Object buildChildNodeMap(Node node) {
+  private static Object getText(Node node) {
     NodeList children = node.getChildNodes();
     if (children.getLength() == 1 && children.item(0).getNodeType() == Node.TEXT_NODE) {
       return children.item(0).getTextContent().trim();
     } else {
       Map<String, Object> childMap = new HashMap<>();
-      buildJsonMap(node, childMap);
+      toMap(node, childMap);
       return childMap;
     }
   }
   
   public static void main(String[] args) throws Exception {
-    Arrays.asList(TestXmlString.values()).stream().forEach(item -> SampleXml.test(item));
+    Arrays.asList(TestXmlString.values()).stream().forEach(item -> {
+      try {
+        SampleXml.convertToMap(item);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    });
   }
 }
