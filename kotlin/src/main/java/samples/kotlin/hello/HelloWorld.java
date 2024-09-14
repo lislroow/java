fb016c9a08f08ceec0ac83b8df7ef890c7ab7391
김명구 <myeonggu.kim@kakao.com>
2024-09-14 13:48:22 +0900

    base

--- kotlin/src/main/java/samples/kotlin/hello/HelloWorld.java
+++ kotlin/src/main/java/samples/kotlin/hello/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
   
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    MyKotlinClass myKotlinClass = new MyKotlinClass();
+//    MyKotlinClass myKotlinClass = new MyKotlinClass();
     
   }
 }
