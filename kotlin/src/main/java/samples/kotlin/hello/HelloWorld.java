f10f2c307c30ebbe3e78f85cf2120494bccbdab7
myeonggu.kim <myeonggu.kim@kakao.com>
2024-09-14 12:48:13 +0900

    kotlin 작업 중

--- kotlin/src/main/java/samples/kotlin/hello/HelloWorld.java
+++ kotlin/src/main/java/samples/kotlin/hello/HelloWorld.java
@@ -1,8 +1,11 @@
 package samples.kotlin.hello;
 
+
 public class HelloWorld {
   
   public static void main(String[] args) {
     System.out.println("Hello, World");
+    MyKotlinClass myKotlinClass = new MyKotlinClass();
+    
   }
 }
