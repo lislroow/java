8a7ba20af3a316058dfedad2d035756538857a5f
myeonggu.kim <hi@mgkim.net>
2024-11-12 21:00:06 +0900

    misc: security 코드 제거

--- boot3x/boot3x-app/src/main/java/spring/sample/Boot3xAppMain.java
+++ boot3x/boot3x-app/src/main/java/spring/sample/Boot3xAppMain.java
@@ -2,11 +2,9 @@ package spring.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableWebSecurity
 public class Boot3xAppMain {
   
   public static void main(String[] args) {
