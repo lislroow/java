372e66dfc1d55bf1bdd702f6dd88e8cdfaba9c6e
myeonggu.kim <hi@mgkim.net>
2024-11-12 11:01:10 +0900

    feature: mybatis 추가

--- boot23/boot23-app/src/main/java/spring/sample/Boot23AppMain.java
+++ boot23/boot23-app/src/main/java/spring/sample/Boot23AppMain.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import spring.sample.common.constant.Constant;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableWebMvc
 @EnableFeignClients(basePackages = {Constant.BASE_PACKAGE})
 public class Boot23AppMain {
   
