475ed09c54120d59ea8bdfca3e8e6442c2267f26
myeonggu.kim <hi@mgkim.net>
2024-11-17 17:23:29 +0900

    misc: FeignClient 타입 oooFeign 으로 변경

--- boot23/boot23-app/src/main/java/spring/sample/app/controller/QualiController.java
+++ boot23/boot23-app/src/main/java/spring/sample/app/controller/QualiController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 import spring.sample.app.enumcode.SOAP_CLIENT_TYPE;
-import spring.sample.app.feign.FeignInternalSoapclientController;
+import spring.sample.app.feign.InternalSoapclientFeign;
 import spring.sample.common.dto.ResponseDto;
 import spring.sample.common.enumcode.RESPONSE_CODE;
 import spring.sample.common.validator.EnumValidator;
@@ -19,12 +19,12 @@ import spring.sample.common.validator.EnumValidator;
 @AllArgsConstructor
 public class QualiController {
   
-  private FeignInternalSoapclientController feignInternalSoapclientController;
+  private InternalSoapclientFeign internalSoapclientFeign;
   
   @PostMapping("/app/v1/quali/{clientType}")
   public ResponseDto<Map<String, Object>> soap(
       @PathVariable @EnumValidator(enumClazz = SOAP_CLIENT_TYPE.class) String clientType) {
-    ResponseDto<Map<String, Object>> result = feignInternalSoapclientController.soap(clientType);
+    ResponseDto<Map<String, Object>> result = internalSoapclientFeign.soap(clientType);
     Map<String, Object> body = null;
     
     if (RESPONSE_CODE.S000.name().equals(result.getHeader())) {
--- boot23/boot23-app/src/main/java/spring/sample/app/feign/InternalSoapclientFeign.java
+++ boot23/boot23-app/src/main/java/spring/sample/app/feign/InternalSoapclientFeign.java
@@ -10,7 +10,7 @@ import spring.sample.common.dto.ResponseDto;
 import spring.sample.config.FeignConfig;
 
 @FeignClient(name = "boot23-soapclient", configuration = {FeignConfig.class})
-public interface FeignInternalSoapclientController {
+public interface InternalSoapclientFeign {
 
   @GetMapping("/internal/soapclient/v1/{clientType}")
   public ResponseDto<Map<String, Object>> soap(@PathVariable String clientType);
