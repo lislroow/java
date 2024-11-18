21f2a988f8575b16cb68e953ec17546c40346866
myeonggu.kim <hi@mgkim.net>
2024-11-18 13:42:04 +0900

    misc: Constant 변수 정리

--- boot3x/boot3x-mybatis/src/main/java/spring/sample/common/constant/Constant.java
+++ boot3x/boot3x-mybatis/src/main/java/spring/sample/common/constant/Constant.java
@@ -8,8 +8,6 @@ public class Constant {
   public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
   public static final String ENCODING_UTF8 = "utf-8";
   public static final String ENABLED = "enabled";
-
-  public static final String X_TOKEN_ID = "X-TOKEN-ID";
   
   public static class DBMS {
     public static final String H2 = "h2";
@@ -19,7 +17,12 @@ public class Constant {
     public static final String POSTGRES = "postgres";
   }
   
-  public static final String SQL_SESSION_FACTORY_BEAN = "SqlSessionFactoryBean";
-  public static final String DAO = "Dao";
+  public static class BEAN {
+    public static final String SQL_SESSION_FACTORY_BEAN = "SqlSessionFactoryBean";
+    public static final String DAO = "Dao";
+  }
   
+  public static class HTTP_HEADER {
+    public static final String X_TOKEN_ID = "X-TOKEN-ID";
+  }
 }
