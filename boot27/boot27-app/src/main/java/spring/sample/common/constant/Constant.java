b80580be236ebb6a80071a651e386d85459690db
김명구 <myeonggu.kim@kakao.com>
2024-12-15 20:04:47 +0900

    misc: uri 정리 - Constant.APP.BASE_URI

--- boot27/boot27-app/src/main/java/spring/sample/common/constant/Constant.java
+++ boot27/boot27-app/src/main/java/spring/sample/common/constant/Constant.java
@@ -8,4 +8,7 @@ public class Constant {
   public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
   public static final String ENCODING_UTF8 = "utf-8";
   
+  public static class APP {
+    public static final String BASE_URI = "/boot27-app";
+  }
 }
