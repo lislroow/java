3aa4e12384595a6342ac2d2b6325fdc312d71d49
김명구 <myeonggu.kim@kakao.com>
2024-12-19 08:05:13 +0900

    ref: (프로젝트 정리) elastic, ... > app

--- boot33/boot33-app/src/main/java/spring/sample/app/controller/ElasticController.java
+++ boot33/boot33-app/src/main/java/spring/sample/app/controller/ElasticController.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import spring.sample.app.service.ElasticService;
 import spring.sample.app.vo.FibonacciVo;
-import spring.sample.constant.Constant;
 
 @RestController
 @RequiredArgsConstructor
