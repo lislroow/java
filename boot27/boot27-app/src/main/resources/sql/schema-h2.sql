03a4fda7895ad46a81d29cec07a632b52907310b
김명구 <myeonggu.kim@kakao.com>
2024-12-29 01:01:06 +0900

    fix: .

--- boot27/boot27-app/src/main/resources/sql/schema-h2.sql
+++ boot27/boot27-app/src/main/resources/sql/schema-h2.sql
@@ -9,18 +9,7 @@ CREATE TABLE IF NOT EXISTS scientist
   create_id INT DEFAULT 1,
   modify_id INT DEFAULT 1
 );
-INSERT INTO scientist (name) VALUES('Galileo Galilei');
-INSERT INTO scientist (name) VALUES('Johannes Kepler');
-INSERT INTO scientist (name) VALUES('Isaac Newton');
-INSERT INTO scientist (name) VALUES('Dmitri Mendeleev');
-INSERT INTO scientist (name) VALUES('Albert Einstein');
-INSERT INTO scientist (name) VALUES('Stephen Hawking');
-INSERT INTO scientist (name) VALUES('Nikola Tesla');
-INSERT INTO scientist (name) VALUES('Niels Bohr');
-INSERT INTO scientist (name) VALUES('Michael Faraday');
-INSERT INTO scientist (name) VALUES('James Clerk Maxwell');
-INSERT INTO scientist (name) VALUES('Alan Turing');
-INSERT INTO scientist (name) VALUES('Richard Feynman');
+
 
 /* manager */
 DROP TABLE IF EXISTS manager;
@@ -34,7 +23,6 @@ CREATE TABLE IF NOT EXISTS manager
   create_id INT DEFAULT 1,
   modify_id INT DEFAULT 1
 );
-INSERT INTO manager (email, passwd) VALUES('myeonggu.kim@kakao.com', '{bcrypt}$2a$10$vY/FCPvzZXkQ8ygnbU5DNuO0kS8FOG/RXw7ZTEtZA4DZSv9xTp2EC');
 
 
 /* access_control */
@@ -56,7 +44,6 @@ CREATE TABLE IF NOT EXISTS card
   issue_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   holder_name VARCHAR(100)
 );
-INSERT INTO card (card_no, issue_date, holder_name) VALUES('1111222233334444', PARSEDATETIME('2024-12-20 10:10:01', 'yyyy-MM-dd HH:mm:ss'), '홍길동');
 
 
 /* card_payment */
@@ -69,5 +56,3 @@ CREATE TABLE IF NOT EXISTS card_payment
   paid_amount INT,
   store_name VARCHAR(100)
 );
-INSERT INTO card_payment (card_no, paid_time, paid_amount, store_name) VALUES('1111222233334444', PARSEDATETIME('2024-12-21 13:37:01', 'yyyy-MM-dd HH:mm:ss'), 11600, '할리스커피당산역점');
-INSERT INTO card_payment (card_no, paid_time, paid_amount, store_name) VALUES('1111222233334444', PARSEDATETIME('2024-12-21 19:34:45', 'yyyy-MM-dd HH:mm:ss'), 21250, '군산오징어당산역점');
