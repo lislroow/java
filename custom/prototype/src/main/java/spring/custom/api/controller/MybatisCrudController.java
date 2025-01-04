fe7ceb07f399dfb0394660a4938678d46e760b64
김명구 <myeonggu.kim@kakao.com>
2025-01-04 12:03:10 +0900

    misc: 코드 정리

--- custom/prototype/src/main/java/spring/custom/api/controller/MybatisCrudController.java
+++ custom/prototype/src/main/java/spring/custom/api/controller/MybatisCrudController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -43,33 +44,24 @@ public class MybatisCrudController {
     return ResponseDto.body(resDto);
   }
   
-  @GetMapping("/v1/mybatis-crud/scientist/{id}")
-  public ResponseDto<MybatisCrudResDto.Scientist> findById(
-      @PathVariable String id) {
-    
-    ScientistVo result = scientistDao.findById(id);
-    MybatisCrudResDto.Scientist resDto = modelMapper.map(result, MybatisCrudResDto.Scientist.class);
-    return ResponseDto.body(resDto);
-  }
+  // @GetMapping("/v1/mybatis-crud/scientists")
+  // public ResponseDto<MybatisCrudResDto.PagedScientistList> findList(
+  //     Pageable pageable) {
+  //   
+  //   PagedList<ScientistVo> result = scientistDao.findList(pageable);
+  //   List<MybatisCrudResDto.Scientist> list = result.stream()
+  //       .map(item -> modelMapper.map(item, MybatisCrudResDto.Scientist.class))
+  //       .collect(Collectors.toList());
+  //   Paged paged = modelMapper.map(result, Paged.class);
+  //   MybatisCrudResDto.PagedScientistList resDto = new MybatisCrudResDto.PagedScientistList();
+  //   resDto.setPaged(paged);
+  //   resDto.setList(list);
+  //   return ResponseDto.body(resDto);
+  // }
   
-  @GetMapping("/v1/mybatis-crud/scientist")
-  public ResponseDto<MybatisCrudResDto.PagedScientistList> findList(
-      Pageable pageable) {
-    
-    PagedList<ScientistVo> result = scientistDao.findList(pageable);
-    List<MybatisCrudResDto.Scientist> list = result.stream()
-        .map(item -> modelMapper.map(item, MybatisCrudResDto.Scientist.class))
-        .collect(Collectors.toList());
-    Paged paged = modelMapper.map(result, Paged.class);
-    MybatisCrudResDto.PagedScientistList resDto = new MybatisCrudResDto.PagedScientistList();
-    resDto.setPaged(paged);
-    resDto.setList(list);
-    return ResponseDto.body(resDto);
-  }
-  
-  @GetMapping("/v1/mybatis-crud/scientist/list/{name}")
-  public ResponseDto<PagedList<ScientistVo>> findListByName(
-      @PathVariable String name, Pageable pageable) {
+  @GetMapping("/v1/mybatis-crud/scientists/search")
+  public ResponseDto<PagedList<ScientistVo>> searchByName(
+      @RequestParam String name, Pageable pageable) {
     
     ScientistVo.FindVo vo = ScientistVo.FindVo.builder()
         .name(name)
@@ -77,6 +69,15 @@ public class MybatisCrudController {
     vo.setPageable(pageable);
     PagedList<ScientistVo> result = scientistDao.findListByName(vo);
     return ResponseDto.body(result);
+  }
+  
+  @GetMapping("/v1/mybatis-crud/scientist/{id}")
+  public ResponseDto<MybatisCrudResDto.Scientist> findById(
+      @PathVariable String id) {
+    
+    ScientistVo result = scientistDao.findById(id);
+    MybatisCrudResDto.Scientist resDto = modelMapper.map(result, MybatisCrudResDto.Scientist.class);
+    return ResponseDto.body(resDto);
   }
   
   @PostMapping("/v1/mybatis-crud/scientist")
