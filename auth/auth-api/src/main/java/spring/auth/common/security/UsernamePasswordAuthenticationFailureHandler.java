0a58225f155f470539c1ff3ea9d8e8b85e32c017
김명구 <myeonggu.kim@kakao.com>
2025-01-07 11:19:40 +0900

    misc: AuthenticationFailureHandler 에서 예외 처리 /error 로 변경

--- auth/auth-api/src/main/java/spring/auth/common/security/UsernamePasswordAuthenticationFailureHandler.java
+++ auth/auth-api/src/main/java/spring/auth/common/security/UsernamePasswordAuthenticationFailureHandler.java
@@ -1,15 +1,12 @@
 package spring.auth.common.security;
 
 import java.io.IOException;
-import java.util.Map;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,14 +16,19 @@ public class UsernamePasswordAuthenticationFailureHandler implements Authenticat
   @Override
   public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
       AuthenticationException exception) throws IOException, ServletException {
-    response.setStatus(HttpStatus.FORBIDDEN.value());
+    request.setAttribute(RequestDispatcher.ERROR_REQUEST_URI, request.getRequestURI());
+    request.setAttribute(RequestDispatcher.ERROR_EXCEPTION, exception);
+    request.setAttribute(RequestDispatcher.ERROR_MESSAGE, exception.getMessage());
+    response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
     
+    /*
     MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
     MediaType jsonMimeType = MediaType.APPLICATION_JSON;
     Map<String, String> message = Map.of("result", "사용자 정보가 일치하지 않습니다.");
     if (converter.canWrite(message.getClass(), jsonMimeType)) {
         converter.write(message, jsonMimeType, new ServletServerHttpResponse(response));
     }
+    */
   }
   
 }
--- custom/common/src/main/java/spring/custom/common/exception/ErrorAttributes.java
+++ custom/common/src/main/java/spring/custom/common/exception/ErrorAttributes.java
@@ -1,16 +1,19 @@
 package spring.custom.common.exception;
 
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.WebRequest;
 
-import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class ErrorAttributes extends DefaultErrorAttributes {
 
   @Override
@@ -23,14 +26,20 @@ public class ErrorAttributes extends DefaultErrorAttributes {
     // 기본: STATUS, ERROR, PATH
     
     Map<String, Object> defaultErrorAttributes = super.getErrorAttributes(webRequest, options);
-
+    
     Map<String, Object> problemDetails = new LinkedHashMap<>();
     problemDetails.put("type", "/error");
+    problemDetails.put("status", defaultErrorAttributes.getOrDefault("status", HttpStatus.INTERNAL_SERVER_ERROR.value()));
     problemDetails.put("title", defaultErrorAttributes.getOrDefault("status", HttpStatus.INTERNAL_SERVER_ERROR.value()));
     problemDetails.put("detail", defaultErrorAttributes.getOrDefault("message", "An unexpected error occurred."));
-    problemDetails.put("status", defaultErrorAttributes.getOrDefault("status", HttpStatus.INTERNAL_SERVER_ERROR.value()));
     problemDetails.put("instance", defaultErrorAttributes.getOrDefault("instance", webRequest.getDescription(false)));
     problemDetails.put("timestamp", LocalDateTime.now());
+    
+    /* for debug */ if (log.isInfoEnabled()) {
+      String trace = defaultErrorAttributes.getOrDefault("trace", "").toString();
+      String first = trace.split("\n")[0];
+      log.error(first);
+    }
     
     Object exception = defaultErrorAttributes.get("exception");
     if (exception != null) {
