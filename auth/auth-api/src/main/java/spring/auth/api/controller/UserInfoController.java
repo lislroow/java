809d857583a823d9770459cf7ba888a29f5ebc5a
김명구 <myeonggu.kim@kakao.com>
2025-01-17 11:57:41 +0900

    fix: 사용자 정보 표시 - myinfo 추가

--- auth/auth-api/src/main/java/spring/auth/api/controller/UserInfoController.java
+++ auth/auth-api/src/main/java/spring/auth/api/controller/UserInfoController.java
@@ -2,6 +2,9 @@ package spring.auth.api.controller;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +24,38 @@ public class UserInfoController {
   final UserInfoDao memberDao;
   final ModelMapper modelMapper;
   
+  @GetMapping("/v1/user/member/myinfo")
+  public UserInfoResDto.MemberInfo memberMyInfo() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (!(authentication instanceof UsernamePasswordAuthenticationToken &&
+        authentication.getPrincipal() instanceof MemberVo)) {
+      throw new AppException(ERROR.A403);
+    }
+    
+    MemberVo result = memberDao.selectMemberById(authentication.getName())
+        .orElseThrow(() -> new AppException(ERROR.A003));
+    UserInfoResDto.MemberInfo resDto = modelMapper.map(result, UserInfoResDto.MemberInfo.class);
+    return resDto;
+  }
+  
+  @GetMapping("/v1/user/manager/myinfo")
+  public UserInfoResDto.MemberInfo managerMyInfo() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (!(authentication instanceof UsernamePasswordAuthenticationToken &&
+        authentication.getPrincipal() instanceof MemberVo)) {
+      throw new AppException(ERROR.A403);
+    }
+    
+    ManagerVo result = memberDao.selectManagerById(authentication.getName())
+        .orElseThrow(() -> new AppException(ERROR.A003));
+    
+    UserInfoResDto.MemberInfo resDto = new UserInfoResDto.MemberInfo();
+    resDto.setId(result.getId());
+    resDto.setLoginId(result.getLoginId());
+    resDto.setNickname(result.getMgrName());
+    return resDto;
+  }
+  
   @GetMapping("/v1/user/member/{id}")
   @PreAuthorize("#id == authentication.name")
   public UserInfoResDto.MemberInfo memberInfo(@PathVariable String id) {
@@ -37,6 +72,7 @@ public class UserInfoController {
         .orElseThrow(() -> new AppException(ERROR.A003));
     
     UserInfoResDto.MemberInfo resDto = new UserInfoResDto.MemberInfo();
+    resDto.setId(result.getId());
     resDto.setLoginId(result.getLoginId());
     resDto.setNickname(result.getMgrName());
     return resDto;
