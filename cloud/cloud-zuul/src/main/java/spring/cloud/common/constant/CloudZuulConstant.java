d832bee6019743b390d75de5506d71243edadcac
김명구 <myeonggu.kim@kakao.com>
2025-01-18 22:46:54 +0900

    clean: 미사용 프로젝트 제거

--- cloud/cloud-zuul/src/main/java/spring/cloud/common/constant/CloudZuulConstant.java
+++
@@ -1,9 +0,0 @@
-package spring.cloud.common.constant;
-
-public class CloudZuulConstant {
-  
-  private CloudZuulConstant() {}
-
-  public static final String BASE_PACKAGE = "spring";
-  
-}
--- cloud/cloud-zuul/src/main/java/spring/cloud/common/enumcode/YN.java
+++
@@ -1,9 +0,0 @@
-package spring.cloud.common.enumcode;
-
-public enum YN {
-  Y, N;
-  
-  public String code() {
-    return this.name();
-  }
-}
